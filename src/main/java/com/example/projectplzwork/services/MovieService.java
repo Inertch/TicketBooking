fedf54fa146016ba659b87df530486f2da25916a
amangeldiyevdiasbek <diasbek2005@gmail.com>
2025-02-07 09:30:07 +0500

    mvnw

--- src/main/java/com/example/projectplzwork/services/MovieService.java
+++ src/main/java/com/example/projectplzwork/services/MovieService.java
@@ -14,22 +14,18 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    // Save Movie
     public Movie saveMovie(Movie movie) {
         return movieRepository.save(movie);
     }
 
-    // Get All Movies
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
-    // Get Movie by ID
     public Optional<Movie> getMovieById(Long id) {
         return movieRepository.findById(id);
     }
 
-    // Delete Movie
     public void deleteMovie(Long id) {
         movieRepository.deleteById(id);
     }
