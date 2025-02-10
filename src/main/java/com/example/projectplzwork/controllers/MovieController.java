807ca8d872f6c86f5283a4c9a288871357c62a3f
Janattie <Sagynbayevazhanat@gmail.com>
2025-02-10 23:28:36 +0500

    Update MovieController.java

--- src/main/java/com/example/projectplzwork/controllers/MovieController.java
+++ src/main/java/com/example/projectplzwork/controllers/MovieController.java
@@ -17,39 +17,47 @@ import java.util.Optional;
 public class MovieController {
 
     @Autowired
-    private MovieService movieService;
+private MovieService movieService;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/admin/add-movie")
-    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
-        movie.setId(null);
+@PreAuthorize("hasRole('ADMIN')")
+@PostMapping("/admin/add-movie")
+public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+    movie.setId(null);
 
-        if (movie.getTickets() != null) {
-            for (Ticket ticket : movie.getTickets()) {
-                ticket.setMovie(movie);
-                if (ticket.getStatus() == null) {
-                    ticket.setStatus(TicketStatus.AVAILABLE);
-                }
+    if (movie.getTickets() != null) {
+        for (Ticket ticket : movie.getTickets()) {
+            ticket.setMovie(movie);
+            if (ticket.getStatus() == null) {
+                ticket.setStatus(TicketStatus.AVAILABLE);
             }
         }
-
-        Movie savedMovie = movieService.saveMovie(movie);
-        return ResponseEntity.ok(savedMovie);
     }
 
-    @GetMapping
-    public List<Movie> getAllMovies() {
-        return movieService.getAllMovies();
-    }
+    Movie savedMovie = movieService.saveMovie(movie);
+    return ResponseEntity.ok(savedMovie);
+}
 
-    @GetMapping("/{id}")
-    public Optional<Movie> getMovieById(@PathVariable Long id) {
-        return movieService.getMovieById(id);
-    }
+@PreAuthorize("hasRole('ADMIN')")
+@PutMapping("/admin/update-movie/{id}")
+public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @Valid @RequestBody Movie updatedMovie) {
+    Movie movie = movieService.updateMovie(id, updatedMovie);
+    return ResponseEntity.ok(movie);
+}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteMovie(@PathVariable Long id) {
-        movieService.deleteMovie(id);
-        return ResponseEntity.ok("Movie deleted successfully.");
-    }
+
+@GetMapping
+public List<Movie> getAllMovies() {
+    return movieService.getAllMovies();
+}
+
+@GetMapping("/{id}")
+public Optional<Movie> getMovieById(@PathVariable Long id) {
+    return movieService.getMovieById(id);
+}
+
+@DeleteMapping("/{id}")
+public ResponseEntity<String> deleteMovie(@PathVariable Long id) {
+    movieService.deleteMovie(id);
+    return ResponseEntity.ok("Movie deleted successfully.");
+}
 }
