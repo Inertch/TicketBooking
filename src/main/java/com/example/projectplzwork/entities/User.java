dfe57d34f7c6f33be2f92689190907cef7653930
Inertch <temirlanchess2@gmail.com>
2025-02-11 02:21:00 +0500

    Update User.java

--- src/main/java/com/example/projectplzwork/entities/User.java
+++ src/main/java/com/example/projectplzwork/entities/User.java
@@ -11,23 +11,5 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(unique = true, nullable = false)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    
 }
